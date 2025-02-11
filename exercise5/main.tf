3b498374b42842db7ca6a9dbe1ec204b22419c2f
AydinTokuslu <aydintokuslu78@gmail.com>
2025-02-11 08:25:51 +0100

    exercise5

---
+++ exercise5/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "docker_instance" {
+    source = "AydinTokuslu/docker-instance/aws"
+    key_name = "mykey"
+}
+
+output "public_ip" {
+  value = module.docker_instance.instance_public_ip
+}
