9af6114eeb87ef63554eb828e538ab96baef5bf2
AydinTokuslu <aydintokuslu78@gmail.com>
2025-02-12 16:43:15 +0100

    exercise7

---
+++ exercise7/security-groups.tf
@@ -0,0 +1,37 @@
+resource "aws_security_group" "all-secgrp" {
+  name        = "all-secgrp"
+  description = "Allow TLS inbound traffic"
+  
+  ingress {
+    description = "allow port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow port 443"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "all-secgrp"
+  }
+}
