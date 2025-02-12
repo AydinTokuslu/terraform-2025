6c93ced156b2f38fb026f74e7a5fa631c095e1e1
AydinTokuslu <aydintokuslu78@gmail.com>
2025-02-12 22:20:34 +0100

    exercise8

---
+++ exercise8-kittens-carousel-static-website-ec2/instance.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "tf-ec2" {
+  ami                     = data.aws_ami.linux2.id
+  instance_type           = var.instance_type
+  key_name = var.instance_keypair
+  user_data = file("${path.module}/userdata.sh")
+  vpc_security_group_ids = [ aws_security_group.all-secgrp.id ]
+  tags = {
+    Name = "kittens-carousel"
+  }
+}
---
+++ exercise8-kittens-carousel-static-website-ec2/variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "region in which AWS Resources to be created"
+  type = string
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 Instance type"
+  type = string
+  default = "t2.micro"
+}
+
+variable "instance_keypair" {
+  description = "keypair for ssh connection"
+  type = string
+  default = "mykey"
+}
