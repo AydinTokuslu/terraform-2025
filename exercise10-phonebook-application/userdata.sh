a57d0084e08c9e2895edda47d9513cea71c107d6
AydinTokuslu <aydintokuslu78@gmail.com>
2025-02-13 16:29:52 +0100

    exercise10

--- exercise10-phonebook-application/userdata.sh
+++ exercise10-phonebook-application/userdata.sh
@@ -16,20 +16,5 @@ cd ..
 python3 phonebook-app.py
 
 
-##! /bin/bash
-# yum update -y
-# yum install python3 -y
-# pip3 install flask
-# pip3 install flask_mysql
-# echo "${MyDBURI}" > /home/ec2-user/dbserver.endpoint
-# 
-# FOLDER="https://$TOKEN@raw.githubusercontent.com/AydinTokuslu/private-my-projects/main/Project-004-Phonebook-Application"
-# curl -s --create-dirs -o "/home/ec2-user/templates/index.html" -L "$FOLDER"/templates/index.html
-# curl -s --create-dirs -o "/home/ec2-user/templates/add-update.html" -L "$FOLDER"/templates/add-update.html
-# curl -s --create-dirs -o "/home/ec2-user/templates/delete.html" -L "$FOLDER"/templates/delete.html
-# curl -s --create-dirs -o "/home/ec2-user/phonebook-app.py" -L "$FOLDER"/phonebook-app.py
-# python3 /home/ec2-user/phonebook-app.py
-
-
 
 
