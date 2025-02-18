e378302a0474dc324e7241018912c396025dbdeb
AydinTokuslu <aydintokuslu78@gmail.com>
2025-02-18 15:10:13 +0100

    updates

--- exercise10-phonebook-application/userdata.sh
+++ exercise10-phonebook-application/userdata.sh
@@ -1,34 +1,18 @@
 #! /bin/bash
 yum update -y
-yum install python3 -y
-yum install git -y
-pip3 install flask
-pip3 install flask_mysql
-echo "${MyDBURI}" > /home/ec2-user/dbserver.endpoint
+yum install -y python3 git 
+pip3 install flask flask_mysql
+
+# Veritabanı bağlantı noktasını kaydet
+echo "${db_endpoint}" > /home/ec2-user/dbserver.endpoint
+
+# Uygulama dizinine gidip projeyi klonla
 cd /home/ec2-user
 git clone https://github.com/AydinTokuslu/contact-list-project.git
 cd contact-list-project
-python3 contact-list-app.py
-MyDBURI: !GetAtt aws_db_instance.my-database-server.endpoint
 
+# Flask uygulamasını arka planda çalıştır
+nohup python3 contact-list-app.py > app.log 2>&1 &
 
-# #! /bin/bash
-# yum update -y
-# yum install python3 -y
-# pip3 install flask
-# pip3 install flask_mysql
-# yum install git -y
-# #echo "${MyDBURI}" > /home/ec2-user/dbserver.endpoint
-# echo "Database Endpoint: ${MyDBURI}" > /home/ec2-user/dbserver.endpoint
-# cd /home/ec2-user
-# FOLDER="https://raw.githubusercontent.com/AydinTokuslu/my-projects/refs/heads/main/aws/Project-004-Phonebook-Application"
-# wget ${FOLDER}/phonebook-app.py
-# mkdir templates && cd templates
-# wget ${FOLDER}/templates/index.html
-# wget ${FOLDER}/templates/add-update.html
-# wget ${FOLDER}/templates/delete.html
-# cd ..
-# python3 phonebook-app.py
-# - MyDBURI: !GetAtt RDSDB.Endpoint.Address
 
 
---
+++ exercise10-phonebook-application/userdata2.sh
@@ -0,0 +1,31 @@
+# #! /bin/bash
+# yum update -y
+# yum install python3 -y
+# pip3 install flask
+# pip3 install flask_mysql
+# yum install git -y
+# #echo "${MyDBURI}" > /home/ec2-user/dbserver.endpoint
+# echo "Database Endpoint: ${MyDBURI}" > /home/ec2-user/dbserver.endpoint
+# cd /home/ec2-user
+# FOLDER="https://raw.githubusercontent.com/AydinTokuslu/my-projects/refs/heads/main/aws/Project-004-Phonebook-Application"
+# wget ${FOLDER}/phonebook-app.py
+# mkdir templates && cd templates
+# wget ${FOLDER}/templates/index.html
+# wget ${FOLDER}/templates/add-update.html
+# wget ${FOLDER}/templates/delete.html
+# cd ..
+# python3 phonebook-app.py
+
+
+#! /bin/bash
+# yum update -y
+# yum install python3 -y
+# yum install git -y
+# pip3 install flask
+# pip3 install flask_mysql
+# echo "${MyDBURI}" > /home/ec2-user/dbserver.endpoint
+# cd /home/ec2-user
+# git clone https://github.com/AydinTokuslu/contact-list-project.git
+# cd contact-list-project
+# python3 contact-list-app.py
+# MyDBURI: !GetAtt aws_db_instance.my-database-server.endpoint
